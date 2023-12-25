9689e090467ba238661f0280fb63de665e729902
DocNITE <docnite0530@gmail.com>
2023-12-25 13:25:19 +0300

    Add files via upload

--- csv2xslx.py
+++ csv2xslx.py
@@ -19,7 +19,9 @@ TODO: Сделать игнор для Ввод, DMI, некоторых пол�
 """
 
 SUM_INFO_DEF = "Суммарная информация"
+PC_NAME_FIELD_DEF = "Имя компьютера"
 FONT_DEF = "Times New Roman"
+
 ROWS_MAX_DEF = 256
 MAIN_TITLE_SIZE_DEF = 20
 TITLE_SIZE_DEF = 14
@@ -27,11 +29,21 @@ CONTENT_SIZE_DEF = 12
 A_ROW_WIDTH_DEF = 11
 B_ROW_WIDTH_DEF = 30
 C_ROW_WIDTH_DEF = 50
+MAX_PRINTERS = 4
+MAX_GPU = 1
+
 IGNORE_TITLES = ["DMI", "Ввод"]
+IGNORE_ITEMS = [
+"DirectX", "Edge", "Internet Explorer", 
+"Дата / Время", "SMART-статус жёстких дисков", 
+"Контроллер хранения данных", "Вход в домен",
+"Коммуникационный порт"]
+
+PINTERS_DEF = "Принтер"
+GPU_DEF = "Видеоадаптер"
+
 FILE_NAME_DEF = sys.argv[1]
 FILES_DIR_DEF = "./"
-
-# os.system("py run.py")
 
 """
 Проверяет является ли массив частью "Суммарной информации"
@@ -64,6 +76,21 @@ def getFileInfo(file_name):
                 continue
             result.append(arr_info)
         return [file_name, result]
+        
+"""
+yeas
+"""
+def checkIgnoreTitle(title):
+    for data in IGNORE_TITLES:
+        if data == title:
+            return True
+    return False
+
+def checkIgnoreItem(item):
+    for data in IGNORE_ITEMS:
+        if data == item:
+            return True
+    return False
 
 """
 Конвертирует полученный массив в excel data для таблицы.
@@ -71,16 +98,33 @@ def getFileInfo(file_name):
 def toFrame(data):
     result = []
     result.append(["", "", ""])
+    pc_name = ""
     old_group = ""
+    curr_printers = 0
+    curr_gpu = 0
     for arr in data:
         print(arr)
         print("\n")
-        if old_group != arr[0]:
+        if old_group != arr[0] and checkIgnoreTitle(arr[0]) != True:
             result.append([arr[0]+":", "", ""])
             old_group = arr[0]
             # continue
-        result.append(["", arr[1], arr[2]])
-    return pd.DataFrame(result)
+        if arr[1] == PC_NAME_FIELD_DEF:
+            pc_name = arr[2]
+        if arr[1] == PINTERS_DEF:
+            curr_printers = curr_printers + 1
+        if arr[1] == GPU_DEF:
+            curr_gpu = curr_gpu + 1
+        # shit statement
+        if checkIgnoreTitle(arr[0]) != True:
+            if checkIgnoreItem(arr[1]) != True:
+                if arr[1] != PINTERS_DEF and arr[2] != GPU_DEF:
+                    result.append(["", arr[1], arr[2]])
+                elif arr[1] == PINTERS_DEF and curr_printers <= MAX_PRINTERS:
+                    result.append(["", arr[1], arr[2]])
+                elif arr[1] == GPU_DEF and curr_gpu <= MAX_GPU:
+                    result.append(["", arr[1], arr[2]])
+    return [pd.DataFrame(result), pc_name]
 
 """
 Записывает данные в excel. Also - добавляет стили и все такое
@@ -88,9 +132,12 @@ def toFrame(data):
 def writeExcel(data, name):
     with pd.ExcelWriter(name, engine="openpyxl") as writer:
         for data_f in data:
-            sheet_n = data_f[0] # todo: shoulde be use PC name from report
             # writing
-            data_fc = toFrame(data_f[1])
+            full_fc = toFrame(data_f[1])
+            pc_name = full_fc[1]
+            # sheet_n = data_f[0] # todo: shoulde be use PC name from report
+            sheet_n = pc_name
+            data_fc = full_fc[0]
             data_fc.to_excel(writer, sheet_name=sheet_n, index=False)
             #todo: also - add title support from pc name 
             # styling
@@ -101,8 +148,11 @@ def writeExcel(data, name):
             sheet.column_dimensions['C'].width = C_ROW_WIDTH_DEF
             border_style = Side(border_style="thick")
             border = Border(top=border_style, left=border_style, right=border_style, bottom=border_style)
+            border_title = Border(bottom=border_style)
+            border_nothing = Border(top=border_style, bottom=border_style)
             alignment = Alignment(horizontal='left', vertical='center', wrap_text=True)
             alignment_a = Alignment(horizontal='left')
+            alignment_title = Alignment(horizontal='center')
             # formating B column 
             for cell in sheet['B']:
                 cell.font = Font(name=FONT_DEF, size=CONTENT_SIZE_DEF)
@@ -135,12 +185,19 @@ def writeExcel(data, name):
             if s_ai != 0:
                 sheet.merge_cells("A"+str(s_ai)+":A"+str(ai-1))
                 s_ai = 0
-            alignment_title = Alignment(horizontal='center')
-            border_title = Border(bottom=border_style)
-            sheet['A1'].value = "There should b ename"
+            sheet['A1'].value = pc_name
             sheet.merge_cells('A1:C1')
             sheet['A1'].font = Font(name=FONT_DEF, size=MAIN_TITLE_SIZE_DEF)
+            # borders
             sheet['A1'].border = border_title
+            sheet['B1'].border = border_title
+            sheet['C1'].border = border_title
+            sheet['A2'].border = border_nothing
+            sheet['A2'].font = Font(name=FONT_DEF, size=MAIN_TITLE_SIZE_DEF)
+            sheet['B2'].border = border_nothing
+            sheet['C2'].border = border_nothing
+            sheet.merge_cells('A2:C2')
+            # alignments 
             sheet['A1'].alignment = alignment_title
             workbook.save(name)
            
